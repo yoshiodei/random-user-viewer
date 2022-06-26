e525492657240ff62257427d521db0052eb829fe
yoshiodei <yoshiodei@yahoo.com>
2022-06-26 23:55:37 +0000

    create empty list component

---
+++ src/components/EmptyList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Empty = () => {
+    return (
+        <div className='empty-list'>
+            <h1>List is empty</h1>
+        </div>
+    );
+}
+
+export default Empty;
