43f5fd3d249b0f766521870027c3ebcfe0459bab
yoshiodei <yoshiodei@yahoo.com>
2022-06-26 23:56:20 +0000

    create load spinner component

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoadingSpinner = () => {
+    return (
+        <div className='spinner-div'>
+            <div className='loading-spinner'>   
+            </div>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
