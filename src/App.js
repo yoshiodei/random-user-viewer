6ef06bec65c06d50c6bd0b459c493c9f420a4c79
yoshiodei <yoshiodei@yahoo.com>
2022-07-03 04:29:52 +0000

    remove fetch function

--- src/App.js
+++ src/App.js
@@ -1,71 +1,27 @@
 import {useState, useEffect} from 'react';
 import DataFilter from './components/DataFilter';
-import EmptyList from './components/EmptyList';
 import Error from './components/Error';
 import LoadingSpinner from './components/LoadingSpinner';
-import Users from './components/Users';
+import UsersList from './components/UsersList';
+import { GET_ALL_USERS } from './endPoints/fetchData';
 import './styles/listStyles.css';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [country,setCountry] = useState('');
+  const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [filteredData, setFilteredData] = useState([]);
-  const [initialData, setInitialData] = useState([]);
 
+  useEffect(() => { GET_ALL_USERS(setUserData,setLoading,setError) },[]);  
 
-// fetch data on page load
-  useEffect(() => {
-    fetch('https://randomuser.me/api/?results=20')
-    .then((res) => {
-        if(!res.ok){
-          setLoading(false);
-          setInitialData([]);
-          setData([]);
-          setError(true)
-          throw Error('Could not fetch data!');
-        }
-        return res.json();
-    })
-    .then((data) => {
-      setInitialData(data.results);
-      setData(data.results);
-      setLoading(false);
-      setError(false);
-    })
-    .catch(() => {
-      setInitialData([]);
-      setData([]);
-      setLoading(false);
-      setError(true);
-    })
-
-    console.log("data",data);
-
-  },[]);
-
-  const switchCountry = (country) => {
-    if(country){
-      let filteredList = initialData.filter(user => user.location.country === country);
-      setFilteredData(filteredList);
-      setData([]);
-    }
-    else {
-      setData(initialData);
-      setFilteredData([]);
-    }
-  
-  }  
+  if (loading) return <LoadingSpinner />;
+  if (error) return <Error />;
 
   return (
     <main className="App">
-      <DataFilter switchCountry={switchCountry} />  
+      <DataFilter setCountry={setCountry} />  
       <section className='main-section'>
-        { loading && <LoadingSpinner /> }
-        { error && <Error /> } 
-        { (filteredData.length === 0 && data.length !== 0) && <Users data={initialData} /> }
-        { (filteredData.length !== 0) && <Users data={filteredData} /> }
-        { (filteredData.length === 0 && data.length === 0 && !loading && !error) && <EmptyList /> }
+        <UsersList country={country} userData={userData} />
       </section>
     </main>
   );
